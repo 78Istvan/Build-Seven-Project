c02065fca1df15055c474cc7d3a8c0e3bdc65994
78Istvan <istvan78juhasz@gmail.com>
2021-10-21 23:15:21 +0000

    Edit html page form functioning fixed

--- app.py
+++ app.py
@@ -164,7 +164,6 @@ def delete_recipe(cook_id):
     return redirect(url_for("get_cooking"))
 
 
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
