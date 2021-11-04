37e5ce8746748d3fe590bfcf28f6a878edcbc04d
78Istvan <istvan78juhasz@gmail.com>
2021-11-04 13:19:19 +0000

    Rating stars added to recipe cards

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
   
-// delete function url 
+// delete function 
 function deleteRecipe(id) {
   var id=$('input.recepy-id').val(),
   base_url = new URL('/', location.href).href,
@@ -19,7 +19,7 @@ function deleteRecipe(id) {
   });
 
 }
-
+// update function 
 function deleteRecipe(id) {
   var id=$('input.recepy-id').val(),
   base_url = new URL('/', location.href).href,
@@ -36,3 +36,34 @@ function deleteRecipe(id) {
   });
 
 }
+// rating stars
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+const ratingResult = document.querySelector(".rating__result");
+
+printRatingResult(ratingResult);
+
+function executeRating(stars, result) {
+   const starClassActive = "rating__star fas fa-star";
+   const starClassUnactive = "rating__star far fa-star";
+   const starsLength = stars.length;
+   let i;
+   stars.map((star) => {
+      star.onclick = () => {
+         i = stars.indexOf(star);
+
+         if (star.className.indexOf(starClassUnactive) !== -1) {
+            printRatingResult(result, i + 1);
+            for (i; i >= 0; --i) stars[i].className = starClassActive;
+         } else {
+            printRatingResult(result, i);
+            for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+         }
+      };
+   });
+}
+
+function printRatingResult(result, num = 0) {
+   result.textContent = `${num}/5`;
+}
+
+executeRating(ratingStars, ratingResult);
